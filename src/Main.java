89851f2c09874fc15f4b17e733b655b42df9957c
VladScv <ddcllrnsbrv@uoc.edu>
2022-06-16 15:34:10 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(productFormatter(" Any cool name  ","21/03/2020"));
+        System.out.println(productFormatter("    Another name  ","21/04"));
+        System.out.println(productFormatter("pATatas Fritas  ","21/03/1918"));
     }
     public static String productFormatter(String productName, String date){
         return (productName.trim().replace(' ','_')+date).toUpperCase();
